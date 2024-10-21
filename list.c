d5c23478435322bb90e36b12de5eb372d5c42358
ost2 <133139140+ost2@users.noreply.github.com>
2024-10-21 13:54:50 +0200

    almost works

--- list.c
+++ list.c
@@ -30,7 +30,7 @@ void list_append(struct list* list_ptr, void* data)
 {
     if (list_ptr->count >= list_ptr->size)
     {
-        void* data_buf = realloc(*list_ptr->data, + list_ptr->size * 2 * list_ptr->data_size);
+        void* data_buf = realloc(*list_ptr->data, list_ptr->data_size + (list_ptr->size * 2) * list_ptr->data_size);
         if (!data_buf)
         {
             fprintf(stderr, "list_append: realloc failed\n");
@@ -41,8 +41,8 @@ void list_append(struct list* list_ptr, void* data)
         *list_ptr->data = data_buf;
         list_ptr->size *= 2;
     }
-    list_ptr->data[(list_ptr->count + 1) * list_ptr->data_size] = data;
     list_ptr->count++;
+    *(int**)list_ptr->data[(list_ptr->count) * list_ptr->data_size] = data;
 }
 
 void* list_remove(struct list* list_ptr, int index)
