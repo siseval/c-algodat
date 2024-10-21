21f858fcb9ab42bf14bb781b9cb1aed13d449211
ost2 <133139140+ost2@users.noreply.github.com>
2024-10-21 13:00:28 +0200

    almost works

--- list.c
+++ list.c
@@ -3,11 +3,20 @@
 
 struct list* list_create(size_t size, size_t data_size)
 {
-    struct list* list_ptr; 
-    void** data = malloc(size * data_size);
-    struct list list = {size, 0, data_size, data};
-    list_ptr = malloc(sizeof(struct list) + size * data_size);
-    *list_ptr = list;
+    struct list* list_ptr = malloc(sizeof(struct list));
+    if (!list_ptr) { return NULL; }
+
+    list_ptr->size = size;
+    list_ptr->count = 0;
+    list_ptr->data_size = data_size;
+    list_ptr->data = malloc((size + 1) * data_size);
+
+    if (!list_ptr->data) 
+    {
+        free(list_ptr);
+        return NULL;
+    }
+
     return list_ptr;
 }
 
@@ -21,11 +30,7 @@ void list_append(struct list* list_ptr, void* data)
 {
     if (list_ptr->count >= list_ptr->size)
     {
-        void** data_buf = realloc(list_ptr->data, list_ptr->size * 2 * list_ptr->data_size);
-        for (int i = 0; i < list_ptr->count; i++)
-        {
-            data_buf[i * list_ptr->data_size] = &*list_ptr->data[i * list_ptr->data_size];
-        }
+        void* data_buf = realloc(*list_ptr->data, + list_ptr->size * 2 * list_ptr->data_size);
         if (!data_buf)
         {
             fprintf(stderr, "list_append: realloc failed\n");
@@ -33,10 +38,10 @@ void list_append(struct list* list_ptr, void* data)
             free(list_ptr);
             return;
         }
-        list_ptr->data = data_buf;
+        *list_ptr->data = data_buf;
         list_ptr->size *= 2;
     }
-    list_ptr->data[list_ptr->count * list_ptr->data_size] = data;
+    list_ptr->data[(list_ptr->count + 1) * list_ptr->data_size] = data;
     list_ptr->count++;
 }
 
@@ -62,6 +67,6 @@ void list_clear(struct list* list_ptr)
 
 void* list_get(struct list* list_ptr, int index)
 {
-    return list_ptr->data + index * list_ptr->data_size;
+    return list_ptr->data + (index + 1) * list_ptr->data_size;
 }
 
